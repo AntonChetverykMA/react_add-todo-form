e004f73cf47b43c34259f9800c7884bd43811292
AntonChetveryk <chetverykanton92@gmail.com>
2020-11-09 22:49:40 +0200

    Updates

---
+++ precache-manifest.3226c8ac552c96d1f8731b74b115179f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4c81b636221c614b00625647fbfd9d3",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "d4b8bc1ba0db833a6baa",
+    "url": "/react_add-todo-form/static/css/main.5664acd7.chunk.css"
+  },
+  {
+    "revision": "32c09743fa023abfb870",
+    "url": "/react_add-todo-form/static/js/2.572236f9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react_add-todo-form/static/js/2.572236f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4b8bc1ba0db833a6baa",
+    "url": "/react_add-todo-form/static/js/main.ca01e463.chunk.js"
+  },
+  {
+    "revision": "4f2dc5553706b5c0675d",
+    "url": "/react_add-todo-form/static/js/runtime-main.cb0096df.js"
+  }
+]);
